21ab764b99a1f04c741750a3109a88c1d7957a55
Alejandro Abdelnur <tucu@tabsdata.com>
2025-06-18 10:30:52 +0200

    [TD-466] Salesforce account setup for tests (#388)
    * data4tests directory with README.md, data file and script to configure and seed the Salesforce
        developer acccount for the Salesforce connector tests.

---
+++ connectors/python/tabsdata_salesforce/tests_tabsdata_salesforce/testing_resources/README.md
@@ -0,0 +1,47 @@
+<!--
+Copyright 2025 Tabs Data Inc.
+-->
+
+# Seeding Data for Salesforce Tests
+
+Set the following environment variables:
+    * SALESFORCE_USERNAME
+    * SALESFORCE_PASSWORD
+    * SALESFORCE_SECURITY_TOKEN
+    * SALESFORCE_INSTANCE_URL
+
+Run the `data-setup.py` script.The script will first delete previously seeded Contact data and then
+it will upload the Contact data from the `data.csv` file (5000 contacts).
+
+*Note:* the Salesforce Developer account has some predefined contacts that cannot be deleted.
+
+### How the Salesforce Developer Edition Account Was Created
+
+In case there is a need to recreate the Salesforce Developer Edition account, follow these steps:
+
+* Go to https://developer.salesforce.com/
+* Loging to the developer account
+* To create the API token: Switch to the Lightning Experience console then go to user’s (avatar top right) 
+Settings, then on the left vertical navigation go to `My Personal Information` => `Reset My Security Token`. 
+The new token will be sent to the email associated with the developer account.
+
+### Giving Bulk Hard Delete permissions to the Salesforce User
+
+* Create a permission set with the "Bulk API Hard Delete" permission:
+`Navigate to Setup` => `Permission Sets`
+* Create a new Permission Set:
+  * Click "New"
+  * Enter a Label (e.g., "Hard Delete Access")
+  * Enter an API Name
+  * Save
+* Add the Hard Delete Permission:
+  * Open your new Permission Set
+  * Click "System Permissions"
+  * Find and check "Bulk API Hard Delete" Permission Allow Sys.
+  * Save
+* Assign the Permission Set:
+  * Go to the Permission Set detail page
+  * Click "Manage Assignments"
+  * Click "Add Assignments"
+  * Select the user(s) you want to grant this permission to
+  * Click "Assign"
---
+++ connectors/python/tabsdata_salesforce/tests_tabsdata_salesforce/testing_resources/data_setup.py
@@ -0,0 +1,74 @@
+#
+#  Copyright 2025. Tabs Data Inc.
+#
+
+import logging
+import os
+
+import pandas as pd
+from simple_salesforce import Salesforce
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def sf_connection():
+    # Connect to Salesforce
+    username = os.getenv("SALESFORCE_USERNAME")
+    password = os.getenv("SALESFORCE_PASSWORD")
+    security_token = os.getenv("SALESFORCE_SECURITY_TOKEN")
+    domain = os.getenv("SALESFORCE_INSTANCE_URL")  # Default to 'login' if not set
+    return Salesforce(
+        username=username,
+        password=password,
+        security_token=security_token,
+        instance_url=domain,
+    )
+
+
+def delete_bulk_contacts(sf):
+    query = "SELECT Contact.Id FROM Contact WHERE Contact.AccountId = null"
+    result = sf.bulk.Contact.query_all(query)
+    record_ids = [{"Id": record["Id"]} for record in result]
+    sf.bulk.Contact.hard_delete(record_ids, batch_size=5000)
+    print(f"Records deleted: {len(record_ids)}")
+
+
+def upload_bulk_contacts(sf, contacts_list):
+    sf.bulk.Contact.insert(contacts_list, batch_size=5000)
+
+
+def upload_csv(sf, data_file):
+    try:
+        df = pd.read_csv(data_file)
+        print(f"Record loaded from file: {len(df)}")
+
+        field_mapping = {
+            "first": "FirstName",
+            "last": "LastName",
+            "email": "Email",
+            "phone": "Phone",
+        }
+
+        contact_list = []
+        for _, row in df.iterrows():
+            contact = {}
+            for csv_field, sf_field in field_mapping.items():
+                if csv_field in row:
+                    contact[sf_field] = row[csv_field]
+            contact_list.append(contact)
+
+        upload_bulk_contacts(sf, contact_list)
+
+        print(f"Records uploaded to salesforce: {len(contact_list)}")
+
+    except Exception as e:
+        logger.error(f"Error processing CSV file: {e}")
+
+
+if __name__ == "__main__":
+    sf = sf_connection()
+    delete_bulk_contacts(sf)
+    upload_csv(sf, "data.csv")
+    print("Data seeded in Salesforce successfully.")
