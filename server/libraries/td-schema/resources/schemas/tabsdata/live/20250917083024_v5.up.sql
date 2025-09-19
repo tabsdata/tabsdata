c80324a7e8363abc83593469e68896a8529a02dc
Joaquin <joaquin@tabsdata.com>
2025-09-19 10:52:39 +0200

    [TD-846] Lock database schema files (#739)

---
+++ server/libraries/td-schema/resources/schemas/tabsdata/live/20250917083024_v5.up.sql
@@ -0,0 +1,10 @@
+--
+--  Copyright 2025 Tabs Data Inc.
+--
+
+UPDATE tabsdata_system
+SET value = '2'
+WHERE name = 'db_version';
+
+ALTER TABLE functions
+    ADD COLUMN ddconnector sssasasTEXT NULL;
